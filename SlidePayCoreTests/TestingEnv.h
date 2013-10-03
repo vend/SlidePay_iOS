177050514f1999cc85c194a5deb81c742c8fa645
Alex Garcia <alex.trashcan@gmail.com>
2013-10-02 19:35:14 -0700

    added an environment file testing

---
+++ SlidePayCoreTests/TestingEnv.h
@@ -0,0 +1,18 @@
+//
+//  TestingEnv.h
+//  SlidePayCore
+//
+//  Created by Alex Garcia on 10/2/13.
+//  Copyright (c) 2013 SlidePay. All rights reserved.
+//
+
+#ifndef SlidePayCore_TestingEnv_h
+#define SlidePayCore_TestingEnv_h
+
+#define SPTESTING_uname @""
+#define SPTESTING_password @""
+#define SPTESTING_magensa @""
+#define SPTESTING_magtek @""
+#define SPTESTING_rambler @""
+
+#endif
